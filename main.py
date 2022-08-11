0ebeb11cc91bbb3106ac14b6aa608dda452a5e27
Tomikone <115artem115@gmail.com>
2022-08-11 19:23:19 +0400

    Exchange calculator

--- main.py
+++ main.py
@@ -172,6 +172,9 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(main())
+    # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    # asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
 
